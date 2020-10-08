3079f181b5a377cf80666b84a62c0ead5fc8ac6b
MarcinDadura <56841909+MarcinDadura@users.noreply.github.com>
2020-10-08 20:55:38 +0200

    init commit

---
+++ core/src/com/dadurek/game/FlappyBird.java
@@ -0,0 +1,40 @@
+package com.dadurek.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.dadurek.game.States.GameStateManager;
+import com.dadurek.game.States.MenuState;
+
+public class FlappyBird extends ApplicationAdapter {
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 900;
+	public static final String TITLE = "Flappy Bird";
+
+	public GameStateManager gsm;
+	private SpriteBatch sb;
+
+	@Override
+	public void create () {
+		sb = new SpriteBatch();
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(0, 0, 1, 1);
+		gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		sb.begin();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(sb);
+
+		sb.end();
+	}
+	
+	@Override
+	public void dispose () {
+		sb.dispose();
+	}
+}
---
+++ core/src/com/dadurek/game/States/MenuState.java
@@ -0,0 +1,25 @@
+package com.dadurek.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
